12ad4f622729858af32444eeba78ba510fabc3ac
Ella Bagchi <ellabagchi@gmail.com>
2025-08-06 15:28:34 -0400

    extract_filter_o3

--- src/scripts/clean.py
+++ src/scripts/clean.py
@@ -1,5 +1,5 @@
-# this script cleans the OMI NO2 data (2005-2024)
-# 1. extracts CloudScreenedTropNO2 data and creates new lat/lon grid
+# this script cleans the OMI NO2 data or OMI O3 data(2005-2024)
+# 1. extracts CloudScreenedTropNO2/ColumnAmountO3 data and creates new lat/lon grid
 # 2. filters lat and lon to my desired region
 # 3. saves this cleaned data as a netCDF
 import h5py
@@ -9,7 +9,9 @@ import os
 
 def extract_data(file_path):
     with h5py.File(file_path, 'r') as f: # read the hdf5 file
-        data_path = '/HDFEOS/GRIDS/ColumnAmountNO2/Data Fields/ColumnAmountNO2TropCloudScreened'
+        # ***change variable below
+        #data_path = '/HDFEOS/GRIDS/ColumnAmountNO2/Data Fields/ColumnAmountNO2TropCloudScreened' # uncomment for NO2
+        data_path = '/HDFEOS/GRIDS/ColumnAmountO3/Data Fields/ColumnAmountO3' # for O3
         data = f[data_path][()].copy()
 
         # below-- reconstruct lat/lon grid because I couldn't access the lat/lon in original file
@@ -27,7 +29,7 @@ def extract_data(file_path):
 
     ds = xr.Dataset( # create a new ds for the data
         {
-            'NO2': (['lat', 'lon'], data)
+            'O3': (['lat', 'lon'], data) # ***change variable here 
         },
         coords={
             'lat': lat,
@@ -37,15 +39,17 @@ def extract_data(file_path):
 
     return ds
 
-def filter_no2(ds):
-    lat_max, lat_min = 41.5, 38
+def filter(ds):
+    lat_max, lat_min = 41.5, 38 
     lon_min, lon_max = -81, -73
     filtered_data = ds.sel(lat=slice(lat_min, lat_max), lon=slice(lon_min, lon_max))
     return filtered_data
 
 def main():
-    base_dir = "/home/ellab/air_pollution/src/data/new_no2" # this is where the new filtered files will go
-    for year in range(2005, 2025):  # inclusive of 2024
+    # ***change variable below
+    #base_dir = "/home/ellab/air_pollution/src/data/new_no2" # this is where the new filtered files will go NO2
+    base_dir = "/home/ellab/air_pollution/src/data/new_o3" # O3
+    for year in range(2006, 2025):  # inclusive of 2024
         year_dir = os.path.join(base_dir, str(year))
         if not os.path.isdir(year_dir):
             continue  # skip if year folder doesn't exist
@@ -55,7 +59,7 @@ def main():
                 file_path = os.path.join(year_dir, filename)
                 try:
                     ds = extract_data(file_path) # extract
-                    filtered_no2 = filter_no2(ds) # filter
+                    filtered_no2 = filter(ds) # filter
 
                     # Output filename
                     output_filename = filename.replace(".he5", "_clean.nc")
