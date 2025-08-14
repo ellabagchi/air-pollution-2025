97a0beca51f13f2983087705ead2ec60380ffd14
Ella Bagchi <ellabagchi@gmail.com>
2025-08-14 17:02:13 -0400

    download_hcho

--- src/scripts/data_download.sh
+++ src/scripts/data_download.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-# this code works for downloading no2 daily data 1 year at a time-- change the year as needed, change variable (no2, 03, hcho) as needed
-for year in 2014; do
+# this code works for downloading daily data 1 year at a time-- change the year as needed, change variable (NO2, O3, HCHO) as needed
+for year in 2013; do
 
 
     # base_url="https://acdisc.gesdisc.eosdis.nasa.gov/data/Aura_OMI_Level3/OMNO2d.003/${year}" # uncomment for NO2-- change all variables accordingly
     # prefix="OMI-Aura_L3-OMNO2d_${year}"
 
-    base_url="https://acdisc.gsfc.nasa.gov/data/Aura_OMI_Level3/OMDOAO3e.003/${year}" #  uncommment for O3
-    prefix="OMI-Aura_L3-OMDOAO3e_${year}"
+    # base_url="https://acdisc.gsfc.nasa.gov/data/Aura_OMI_Level3/OMDOAO3e.003/${year}" #  uncommment for O3
+    # prefix="OMI-Aura_L3-OMDOAO3e_${year}"
 
-    tmp_dir="./omi_o3_tmp_${year}"
-    target_dir="/home/ellab/air_pollution/src/data/new_o3/${year}"
+    base_url="https://acdisc.gsfc.nasa.gov/data/Aura_OMI_Level2G/OMHCHOG.003/${year}" #  uncommment for HCHO
+    prefix="OMI-Aura_L2G-OMHCHOG_${year}"
+
+    tmp_dir="./omi_hcho_tmp_${year}"
+    target_dir="/home/ellab/air_pollution/src/data/new_hcho/${year}"
 
 
     mkdir -p "$tmp_dir"
@@ -35,7 +38,7 @@ for year in 2014; do
                 y=${datetag:0:4}
                 m=${datetag:4:2}
                 d=${datetag:6:2}
-                new_filename="omi_o3_${y}${m}${d}.he5"
+                new_filename="omi_hcho_${y}${m}${d}.he5"
                 mv "$file" "$target_dir/$new_filename"
                 echo "Saved: $target_dir/$new_filename"
             else
